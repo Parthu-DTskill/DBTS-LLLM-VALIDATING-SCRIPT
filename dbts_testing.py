856134dda70b6a636bb06278d188e56d90a6ffa1
ParthaSarathi <parthuprince112@gmail.com>
2025-07-01 16:22:47 +0530

    changed the llm to llama

--- dbts_testing.py
+++ dbts_testing.py
@@ -10,6 +10,7 @@ from langchain_community.agent_toolkits import SQLDatabaseToolkit, SparkSQLToolk
 
 time = datetime.now().strftime("%H:%M:%S")
 
+
 class LLMTestingClient:
     API_ENDPOINTS = {
         "login": "https://vistaai-dev-api.dtskill.com/api/auth/login",
@@ -19,16 +20,20 @@ class LLMTestingClient:
         self.token = None
         self.scenario_id = scenario_id
         self.client = Opik()
-        self.dataset = self.client.get_or_create_dataset(name="DBTS Evaluation Dataset", description="Dataset for evaluating DBTS scenarios")
-        self.hallucination_metric = Hallucination(model=GenericCompatibleModel(model_name="gemma2-9b-it", api_key="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", model_provider="groq"))
-        self.answerRelevance_metric = AnswerRelevance(model=GenericCompatibleModel(model_name="gemma2-9b-it", api_key="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", model_provider="groq"))
+        self.dataset = self.client.get_or_create_dataset(
+            name="DBTS Evaluation Dataset", description="Dataset for evaluating DBTS scenarios")
+        self.hallucination_metric = Hallucination(model=GenericCompatibleModel(
+            model_name="llama3-70b-8192", api_key="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", model_provider="groq"))
+        self.answerRelevance_metric = AnswerRelevance(model=GenericCompatibleModel(
+            model_name="llama3-70b-8192", api_key="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", model_provider="groq"))
 
     def login(self, email, password):
         payload = {
             "email": email,
             "password": password
         }
-        self.token = self.make_api_request("POST", "login", payload).get("token")
+        self.token = self.make_api_request(
+            "POST", "login", payload).get("token")
 
     def make_api_request(self, request_method, api_name, payload=None, custom_url=None):
         url = custom_url if custom_url else self.API_ENDPOINTS[api_name]
@@ -41,11 +46,13 @@ class LLMTestingClient:
             headers['Authorization'] = f"Bearer {self.token}"
 
         if request_method.upper() == "GET":
-            response = requests.request(url=url, method=request_method, headers=headers)
+            response = requests.request(
+                url=url, method=request_method, headers=headers)
         else:
             data = json.dumps(payload) if payload else None
             print(f"payload for {api_name} is {data}")
-            response = requests.request(url=url, method=request_method, headers=headers, data=data)
+            response = requests.request(
+                url=url, method=request_method, headers=headers, data=data)
 
         print(f"Response for {api_name}: {response.text}")
         return response.json()
@@ -80,8 +87,10 @@ class LLMTestingClient:
             experiment_name=time
         )
 
+
 if __name__ == "__main__":
-    client = LLMTestingClient(scenario_id="603937b4-d5b8-4d04-86d7-a59ddcd97f1d")
+    client = LLMTestingClient(
+        scenario_id="603937b4-d5b8-4d04-86d7-a59ddcd97f1d")
     client.login(email="parthuprince112@gmail.com", password="mkml")
 #    client.add_samples()
     client.evaluate_data()
